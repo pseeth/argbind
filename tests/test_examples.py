7ff31f78dab98c9f461bc2b5819c91e61ebdf63d
pseeth <prem@descript.com>
2022-09-08 23:25:53 -0700

    Nested .yml works.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -89,13 +89,6 @@ def test_example(path):
 
 def test_yaml_example():
     added_args = [
-        '--args.load=examples/yaml/conf/base.yml',
-        '--args.load=examples/yaml/conf/exp1.yml',
-        '--args.load=examples/yaml/conf/exp2.yml',
-        '--args.load=examples/yaml/conf/exp3.yml',
-        '--args.load=examples/yaml/conf/exp4.yml',
-    ]
-    added_args = [
         {'env': {}, 'flags': ['--args.load=examples/yaml/conf/base.yml']},
         {'env': {}, 'flags': ['--args.load=examples/yaml/conf/exp1.yml']},
         {'env': {}, 'flags': ['--args.load=examples/yaml/conf/exp2.yml']},
@@ -117,6 +110,21 @@ def test_yaml_example():
             stderr=subprocess.PIPE,
             env=environ
         )
+        output = output.stdout.decode('utf-8')
+
+        _path = path.split('examples/')[-1] + f'.run{i}'
+        output_path = regression_path / _path
+        check(output, output_path)
+
+def test_nested_yaml_example():
+    added_args = [
+        {"flags": ["--args.load=examples/nested_yaml/conf/nested.yml"]},
+    ]
+
+    path = str(examples_path / 'nested_yaml' / 'main.py')
+    for i, add_arg in enumerate(added_args):      
+        cmd = [f"python", path] + add_arg['flags'] 
+        output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output = output.stdout.decode('utf-8')
 
         _path = path.split('examples/')[-1] + f'.run{i}'
